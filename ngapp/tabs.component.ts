da19fd33d5fd51aab180055451d793bc330ea5f7
csharper2010 <csharper2010@googlemail.com>
2017-01-21 16:52:42 +0100

    Focus und Pfeiltasten für Angular2-Tab

--- ngapp/tabs.component.ts
+++ ngapp/tabs.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ContentChildren, QueryList, AfterContentInit } from '@angular/core';
+import { Component, Input, ContentChildren, QueryList, HostListener, ViewChildren, ElementRef, AfterContentInit, AfterViewChecked } from '@angular/core';
 
 import { TabComponent } from './tab.component';
 
@@ -6,8 +6,8 @@ import { TabComponent } from './tab.component';
     moduleId: module.id,
     selector: 'ng-tabs',
     template: `
-        <ul class="nav-tabs">
-            <li *ngFor="let tab of tabs; let i = index" (click)=selectTab(i) [class.active]="isActiveTab(i)">
+        <ul class="nav-tabs" (keydown)=handleKeyPress($event)>
+            <li #tabHeader *ngFor="let tab of tabs; let i = index" (click)=selectTab(i) [class.active]="isActiveTab(i)" [tabindex]="isActiveTab(i) ? 0 : -1">
                 {{tab.title}}
             </li>
         </ul>
@@ -16,19 +16,22 @@ import { TabComponent } from './tab.component';
         </div>
         `
 })
-export class TabsComponent implements AfterContentInit {
+export class TabsComponent implements AfterContentInit, AfterViewChecked {
     @Input("activeTab")
     activeTabIndex: number;
+
     @ContentChildren(TabComponent)
     tabs: QueryList<TabComponent>;
 
-    get activeTab(): TabComponent {
-        return this.tabs.toArray()[this.activeTabIndex];
-    }
-
+    @ViewChildren("tabHeader")
+    tabHeaders: QueryList<ElementRef>;
 
     constructor() {
         this.activeTabIndex = 0;
+    }
+
+    get activeTab(): TabComponent {
+        return this.tabs.toArray()[this.activeTabIndex];
     }
 
     ngAfterContentInit() {
@@ -38,13 +41,35 @@ export class TabsComponent implements AfterContentInit {
         this.selectTab(this.activeTabIndex);
     }
 
+    ngAfterViewChecked() {
+    }
+
     isActiveTab(index: number) {
         return index == this.activeTabIndex;
     }
-    selectTab(index: number) {
+
+    selectTab(index: number, focusActiveTab?: boolean) {
         this.activeTabIndex = index;
         this.tabs.forEach((tab, index) => {
             tab.isActive = index == this.activeTabIndex;
         });
+    }
+
+    handleKeyPress(event: KeyboardEvent) {
+        if (event.keyCode == 38 || event.keyCode == 37) {
+            if (this.activeTabIndex > 0) {
+                this.selectTab(this.activeTabIndex - 1);
+            }
+            let element = this.tabHeaders.toArray()[this.activeTabIndex].nativeElement;
+            setTimeout(element.focus());
+            event.preventDefault();
+        } else if (event.keyCode == 40 || event.keyCode == 39) {
+            if (this.activeTabIndex < this.tabs.length - 1) {
+                this.selectTab(this.activeTabIndex + 1);
+            }
+            let element = this.tabHeaders.toArray()[this.activeTabIndex].nativeElement;
+            setTimeout(element.focus());
+            event.preventDefault();
+        }
     }
 }
